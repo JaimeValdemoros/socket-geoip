3686289cff97f339261f0c2affe5986ac210201e
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-08-20 23:38:42 +0000

    Pull in listenfd and anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use listenfd::ListenFd;
+
+fn main() -> anyhow::Result<()> {
+    let mut listenfd = ListenFd::from_env();
+    let _socket = listenfd.take_unix_listener(0)?;
+    Ok(())
+}
