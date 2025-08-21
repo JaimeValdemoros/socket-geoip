de623361d2758625542006b3ff2c9a2dfa11ac5b
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-08-21 00:38:50 +0000

    Implement fcgi

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+use std::io::Write;
+use std::os::fd::AsRawFd;
+
 use listenfd::ListenFd;
 
 fn main() -> anyhow::Result<()> {
     let mut listenfd = ListenFd::from_env();
-    let _socket = listenfd.take_tcp_listener(0)?;
+    let socket = listenfd.take_tcp_listener(0)?.unwrap();
+    fastcgi::run_raw(
+        |mut req| {
+            write!(
+                &mut req.stdout(),
+                "Content-Type: text/plain\n\nHello, world!"
+            )
+            .unwrap_or(());
+        },
+        socket.as_raw_fd(),
+    );
     Ok(())
 }
