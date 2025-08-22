307b6a26ea260814ba0e1988be5a1352460dcd15
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-08-22 22:49:30 +0000

    Scrap musl build

--- default.nix
+++ default.nix
@@ -7,12 +7,11 @@
 }: let
   arch = lib.head (lib.strings.splitString "-" system);
   # https://github.com/nix-community/naersk/blob/master/examples/static-musl/flake.nix
-  toolchain = with fenix.packages.${system};
-    combine [
-      stable.rustc
-      stable.cargo
-      targets."${arch}-unknown-linux-musl".stable.rust-std
-    ];
+  toolchain = fenix.packages.${system}.stable.withComponents [
+    "cargo"
+    "clippy"
+    "rust-src"
+  ];
   naersk' = naersk.lib.${system}.override {
     cargo = toolchain;
     rustc = toolchain;
@@ -20,8 +19,4 @@
 in
   naersk'.buildPackage {
     src = ./.;
-    # Tells Cargo that we're building for musl.
-    CARGO_BUILD_TARGET = "${arch}-unknown-linux-musl";
-    # (see: https://github.com/rust-lang/rust/issues/79624#issuecomment-737415388)
-    CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
   }
